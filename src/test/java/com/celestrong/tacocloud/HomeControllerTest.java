87e8bed424c7078a4947234693798e5075ed6bd2
celeStrong <1269504317@qq.com>
2024-09-13 17:45:23 +0800

    =搭建home页面和使用@WebMvcTest进行mock测试

---
+++ src/test/java/com/celestrong/tacocloud/HomeControllerTest.java
@@ -0,0 +1,26 @@
+package com.celestrong.tacocloud;
+
+
+import com.celestrong.tacocloud.controller.HomeController;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@WebMvcTest(HomeController.class)
+public class HomeControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Test
+    public void testHomePage() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string(Matchers.containsString("Welcome to...")))
+                .andExpect(MockMvcResultMatchers.view().name("home"));
+    }
+
+}
